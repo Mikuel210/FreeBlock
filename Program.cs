ac4f5027ede3963c8bc37b9a268096bdcf0a4331
Miguel <loranvallsmiguel@gmail.com>
2026-04-03 00:37:43 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System.IO;
+
+// Test writing to hosts file
+using StreamWriter file = new("/etc/hosts");
+file.WriteLine("0.0.0.0 youtube.com");
+
+Console.WriteLine("YouTube is now blocked!");
